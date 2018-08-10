592a304df96d0a876a4029b726088f2c6ef225c7
@mdo <dorobisz.mateusz@o2.pl>
2018-08-10 13:22:16 +0200

    commit #3

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const createError = require('http-errors');
 const path = require('path');
 const session = require('express-session');
-const crypto = require('crypto');
+const logger = require('morgan');
+
 const bodyParser = require('body-parser');
-const user = require('./routes/user')
+const cookieParser = require('cookie-parser');
 const hbs = require('hbs');
+
+// Routes:
+const user = require('./routes/user')
+// end Routes
 
 const app = express();
 
-
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 app.use(session({
     name: 'JSESSION',
@@ -20,19 +28,34 @@ app.use(session({
         secure: true
     }
 }));
+// registry use:
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.set('views', path.join(__dirname, 'views'));
-
-
+// Depenency:
 app.use('/user', user);
 
+// index page:
 app.get('/', (req, res)=>{
     res.send('działa index');
 });
 
-app.get('*', (req, res)=>{
-    res.send("error");
+// 404 
+app.use(function(req, res, next){
+    next(createError(404));
 });
+
+// 500
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });  
 
 const port = process.env.port || 4200;
 
